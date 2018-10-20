dd8a6066fc43980bd817935c2ad96183fe1d6973
MaksimPryvolnev <pryvolnev.m@gmail.com>
2018-10-20 13:09:36 +0300

    Implement recursion instead of for loop

--- js/script.js
+++ js/script.js
@@ -5,26 +5,28 @@ const imageW = document.querySelector("input[name=w]");
 const imageH = document.querySelector("input[name=h]");
 const searchQ = document.querySelector("input[name=q]");
 
-async function renderGalleryItem(w,h,q){
-   await fetch(`https://source.unsplash.com/${w}x${h}/?${q}`) 
-  .then((response)=> {
-    let galleryItem = document.createElement('div');
-    galleryItem.classList.add('gallery-item');
-    galleryItem.innerHTML = `
-      <img class="gallery-image" src="${response.url}" alt="gallery image"/>
-    `
-    unsplashSection.appendChild(galleryItem);
-  })
+function renderGalleryItem(w,h,q, numItemsToGenerate){
+    if(numItemsToGenerate && numItemsToGenerate > 0){
+        fetch(`https://source.unsplash.com/${w}x${h}/?${q}`) 
+       .then((response)=> {
+         let galleryItem = document.createElement('div');
+         galleryItem.classList.add('gallery-item');
+         galleryItem.innerHTML = `
+           <img class="gallery-image" src="${response.url}" alt="gallery image"/>
+         `
+         unsplashSection.appendChild(galleryItem);
+         renderGalleryItem(imageW.value,imageH.value,q, numItemsToGenerate-1);
+        })
+    }
 }
+
+
 if(searchForm.addEventListener){
-    searchForm.addEventListener("submit",(e) => {
+    searchForm.addEventListener("submit",async (e) => {
         e.preventDefault();
         let q = searchQ.value.split(" ").join(",");
         const numItemsToGenerate = document.querySelector("input[name=number]").value; //how many gallery items you want on the screen
-        for(let i=0;i<numItemsToGenerate;i++){
-            console.log(imageW.value,imageH.value,q);
-            renderGalleryItem(imageW.value,imageH.value,q);
-        }
+        await renderGalleryItem(imageW.value,imageH.value,q, numItemsToGenerate);
     })
 }
 document.getElementById("year").innerHTML = year;
