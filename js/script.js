a7533bcfbf8f8d5ee72c95097a266e787240b455
MaksimPryvolnev <pryvolnev.m@gmail.com>
2018-10-21 13:44:22 +0300

    Disable cache

--- js/script.js
+++ js/script.js
@@ -5,26 +5,26 @@ const imageW = document.querySelector("input[name=w]");
 const imageH = document.querySelector("input[name=h]");
 const searchQ = document.querySelector("input[name=q]");
 
-async function renderGalleryItem(w,h,q){
-   await fetch(`https://source.unsplash.com/${w}x${h}/?${q}`) 
+renderGalleryItem = (w,h,q) => {
+   fetch(`https://source.unsplash.com/${w}x${h}/?${q}`, {cache: "no-store"}) 
   .then((response)=> {
     let galleryItem = document.createElement('div');
     galleryItem.classList.add('gallery-item');
     galleryItem.innerHTML = `
       <img class="gallery-image" src="${response.url}" alt="gallery image"/>
-    `
+    `;
     unsplashSection.appendChild(galleryItem);
   })
 }
-if(searchForm.addEventListener){
-    searchForm.addEventListener("submit",(e) => {
-        e.preventDefault();
-        let q = searchQ.value.split(" ").join(",");
-        const numItemsToGenerate = document.querySelector("input[name=number]").value; //how many gallery items you want on the screen
-        for(let i=0;i<numItemsToGenerate;i++){
-            console.log(imageW.value,imageH.value,q);
-            renderGalleryItem(imageW.value,imageH.value,q);
-        }
-    })
-}
+
+searchForm.addEventListener("submit",(e) => {
+    e.preventDefault();
+    let q = searchQ.value.split(" ").join(",");
+    const numItemsToGenerate = document.querySelector("input[name=number]").value; //how many gallery items you want on the screen
+    for(let i=0;i<numItemsToGenerate;i++){
+        console.log(imageW.value,imageH.value,q);
+        renderGalleryItem(imageW.value,imageH.value,q);
+    }
+});
+
 document.getElementById("year").innerHTML = year;
