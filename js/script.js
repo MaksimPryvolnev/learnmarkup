04d905445439c2bbac1ad57d4bfc25a0b31d9078
MaksimPryvolnev <pryvolnev.m@gmail.com>
2018-10-21 13:41:13 +0300

    Disable cache

--- js/script.js
+++ js/script.js
@@ -7,26 +7,25 @@ const searchQ = document.querySelector("input[name=q]");
 
 function renderGalleryItem(w,h,q, numItemsToGenerate){
     if(numItemsToGenerate && numItemsToGenerate > 0){
-        fetch(`https://source.unsplash.com/${w}x${h}/?${q}`) 
-       .then((response)=> {
-         let galleryItem = document.createElement('div');
-         galleryItem.classList.add('gallery-item');
-         galleryItem.innerHTML = `
-           <img class="gallery-image" src="${response.url}" alt="gallery image"/>
-         `
-         unsplashSection.appendChild(galleryItem);
-         renderGalleryItem(imageW.value,imageH.value,q, numItemsToGenerate-1);
-        })
+        renderGalleryItem(imageW.value,imageH.value,q, numItemsToGenerate-1);
+        fetch(`https://source.unsplash.com/${w}x${h}/?${q}`, {cache: "no-store"})
+        .then((response)=> {
+            console.dir(response);
+            let galleryItem = document.createElement('div');
+            galleryItem.classList.add('gallery-item');
+            galleryItem.innerHTML = `
+            <img class="gallery-image" src="${response.url}" alt="gallery image"/>
+            `;
+            unsplashSection.appendChild(galleryItem);
+        });
     }
 }
 
+searchForm.addEventListener("submit",(e) => {
+    e.preventDefault();
+    let q = searchQ.value.split(" ").join(",");
+    const numItemsToGenerate = document.querySelector("input[name=number]").value; //how many gallery items you want on the screen
+    renderGalleryItem(imageW.value,imageH.value,q, numItemsToGenerate);
+});
 
-if(searchForm.addEventListener){
-    searchForm.addEventListener("submit",async (e) => {
-        e.preventDefault();
-        let q = searchQ.value.split(" ").join(",");
-        const numItemsToGenerate = document.querySelector("input[name=number]").value; //how many gallery items you want on the screen
-        await renderGalleryItem(imageW.value,imageH.value,q, numItemsToGenerate);
-    })
-}
 document.getElementById("year").innerHTML = year;
